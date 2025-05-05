19fb756ef2189ae9b7f4e75d411530a8f441337d
bnam91 <darling_91@naver.com>
2025-05-05 12:48:49 +0900

    쿠팡메뉴 생성

---
+++ src/js/tabs/tab4/coupang.js
@@ -0,0 +1,11 @@
+/**
+ * 쿠팡 페이지 관리를 위한 JavaScript 모듈
+ */
+export function initPage() {
+    console.log('쿠팡 페이지 초기화');
+    initEventListeners();
+}
+
+function initEventListeners() {
+    // 여기에 이벤트 리스너를 추가할 예정입니다
+} 
